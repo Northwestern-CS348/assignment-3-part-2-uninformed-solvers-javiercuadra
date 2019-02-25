1919cca5481fde70164823995282341f006e1392
Javier Francisco Cuadra <32556567+javiercuadra@users.noreply.github.com>
2019-02-24 21:43:41 -0600

    Update game_master.py

--- game_master.py
+++ game_master.py
@@ -7,11 +7,9 @@ class GameMaster(object):
 
     """
     Abstract parent class of game masters. Must not be instantiated directly.
-
     A game master maintains the state of a game, produces summary of the current state,
     handles external request to make a move (and make the transition between game state),
     and determine if the winning condition has been achieved.
-
     Attributes:
         kb (KnowledgeBase): the Knowledge Base object that keeps track of the rules and game state
         moveableQuery (Fact): the Fact object with which a Game Master queries for all MOVABLE statements
@@ -32,9 +30,7 @@ class GameMaster(object):
         Create the Fact object that could be used to query
         the KB of the presently available moves. This function
         is called once per game.
-
         Must be overridden by each non-abstract children classes.
-
         Returns:
              A Fact object that could be used to query the currently available moves
         """
@@ -44,10 +40,8 @@ class GameMaster(object):
     def isMovableLegal(self, movable_statement):
         """
         Checks if a MOVABLE statement is a legal move in the current game state.
-
         Args:
             movable_statement: A Statement object that may or may not contain a presently viable move
-
         Returns:
             True if the move is legal; False otherwise.
         """
@@ -58,12 +52,9 @@ class GameMaster(object):
         Takes a MOVABLE statement and makes the corresponding move. This will
         result in a change of the game state, and therefore requires updating
         the KB in the Game Master.
-
         Must be overridden by each non-abstract children classes.
-
         Args:
             movable_statement: A Statement object that contains one of the currently viable moves
-
         Returns:
             None
         """
@@ -76,12 +67,9 @@ class GameMaster(object):
         input MOVABLE statement. This function is used to
         undo the effect of a move. Useful in backtracking
         to an older game state.
-
         Must be overridden by each non-abstract children classes.
-
         Args:
             movable_statement: A Statement object that contains one of the previously viable moves
-
         Returns:
             None
         """
@@ -91,9 +79,7 @@ class GameMaster(object):
     def getGameState(self):
         """
         Returns a representation of the game in the current state.
-
         Must be overridden by each non-abstract children classes.
-
         Returns:
             A Tuple of Tuples that represent the game state
         """
@@ -102,25 +88,28 @@ class GameMaster(object):
 
     def getMovables(self):
         """
+        Returns a list of MOVABLE statements that represent the moves currently available.
+        
+        The output Statements will be sorted in ascending orders, first order by the text of predicates,
+        then by each Term in the Statement.
         Returns:
             A list of MOVABLE statements indicating the presently viable moves, if at least one is present;
             False otherwise.
         """
         listOfBindings = self.kb.kb_ask(self.moveableQuery)
         if listOfBindings:
-            return [instantiate(self.moveableQuery.statement,bindings)\
-                     for bindings in listOfBindings]
+            statements = [instantiate(self.moveableQuery.statement,bindings) for bindings in listOfBindings]
+            statements.sort()
+            return statements
         else:
             return listOfBindings
 
     def read(self, file_name, path=TXTS_DIRECTORY_PATH):
         """
         Read and assert all Facts and Rules from a file in the KB.
-
         Args:
             file_name: the name of the file
             path: path to the directory that contains said file
-
         Returns:
             None
         """
@@ -132,11 +121,9 @@ class GameMaster(object):
         """
         Take two lists of Facts in string form, parse them, and save them
         for checking if the winning condition is met.
-
         Args:
             required: a list of strings that specify the Facts that must be present when the winning condition is met
             forbidden: a list of strings that specify the Facts that must NOT be present when the winning condition is met
-
         Returns:
             None
         """
@@ -151,10 +138,8 @@ class GameMaster(object):
         """
         Check if the winning condition is met, with the list of Required Facts
         and the list of forbidden Facts specified by the setWinningCondition method.
-
         Returns:
             True if winning condition is met; False otherwise
-
         Raises:
             Uninitialized Exception: Raises exception if the winning conditions were not set by setWinningCondition
         """
